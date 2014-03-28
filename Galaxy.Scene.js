cb288111e7548b17eed8c114b1e23caf63cb9162
il-hyun Cho <ssdosso@naver.com>
2014-03-28 13:37:23 +0900

    Update Galaxy.Scene.js

--- Galaxy.Scene.js
+++ Galaxy.Scene.js
@@ -1,6 +1,6 @@
 /**
  * Created with JetBrains WebStorm.
- * User: Monoshift
+ * User: Cho il hyun
  * Date: 13. 10. 11
  * Time: 오전 11:00
  * To change this template use File | Settings | File Templates.
